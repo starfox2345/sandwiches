01b0b162f92ac03240547d4d856d45412228c14b
yatsun <yatsunlam25@gmail.com>
2021-08-08 02:02:31 -0400

    custom method for name, temperature, condition, price

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,5 +1,5 @@
 class Sandwich < ApplicationRecord
-    def name_and_temperature
-
+    def name_temperature_condition_price
+        "#{self.name} - #{self.temperature} - #{self.condition} - $#{self.price}"
     end
 end
