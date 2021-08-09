43236bc889e1e14017530969fee1dc1b298186c6
yatsun <yatsunlam25@gmail.com>
2021-08-09 02:58:27 -0400

    added too many sandwiches validation

--- app/models/concerns/temperature_name_validator.rb
+++ app/models/concerns/temperature_name_validator.rb
@@ -1,4 +1,4 @@
-class NameCapitalizeValidator < ActiveModel::EachValidator
+class TemperatureNameValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
         if value.split(" ").length > 1
             record.errors.add(attribute, message: "Can only be 1 word")
--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -2,7 +2,8 @@ class Sandwich < ApplicationRecord
     validates :condition, presence: true
     validates :name, presence: true, uniqueness: {scope: [:name]}, length: {minimum: 2, message: "Name needs to be longer than 2 letters"}
     validates :price, numericality: {greater_than: 0, lesser_than: 100}
-    validates :temperature, presence: true, name_capitalize: true
+    validates :temperature, presence: true, temperature_name: true
+    validate :too_many_sandwiches
     
 
     def name_temperature
@@ -11,5 +12,11 @@ class Sandwich < ApplicationRecord
 
     def self.most_expensive
         self.order(price: :desc)
+    end
+
+    def too_many_sandwiches
+        if Sandwich.all.length >= 10
+            errors.add(:base, :invalid, message: "You have too many sandwiches")
+        end
     end
 end
