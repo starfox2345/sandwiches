61584026ffca8bd2e134598e1f19e7b702733ee1
yatsun <yatsunlam25@gmail.com>
2021-08-09 01:16:39 -0400

    added validations for name, condition, temperature, price

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,9 +1,9 @@
 class Sandwich < ApplicationRecord
     validates :condition, presence: true
-    validates :name, presence: true, length: {minimum: 2}
+    validates :name, presence: true, length: {minimum: 2, message: "Name needs to be longer than 2 letters"}
     validates :price, numericality: {greater_than: 0, lesser_than: 100}
-    validates :temperature, presence: true
-
+    validates :temperature, presence: true, uniqueness: {scope: [:name, :price]}
+    
 
     def name_temperature
         "#{self.name} - #{self.temperature}"
