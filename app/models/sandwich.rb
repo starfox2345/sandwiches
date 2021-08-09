8362234683968fe600a0d294d2765152cf06d217
yatsun <yatsunlam25@gmail.com>
2021-08-08 20:01:17 -0400

    using number_to_currency instead of $ sign

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -3,14 +3,6 @@ class Sandwich < ApplicationRecord
         "#{self.name} - #{self.temperature}"
     end
 
-    # def self.order_by_price
-    #     self.order(:price)
-    # end
-
-    # def self.most_expensive
-    #     self.where("price > 4")
-    # end
-
     def self.most_expensive
         self.order(price: :desc)
     end
