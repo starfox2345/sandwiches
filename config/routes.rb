efdf332d99417f0eb64d9f5fa92c436610ee885f
yatsun <yatsunlam25@gmail.com>
2021-08-11 00:17:51 -0400

    added github route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/sandwiches/most_expensive', to: 'sandwiches#most_expensive'
 
+  get 'auth/github', as: "github_login"
+  get '/auth/:provider/callback', to: "sessions#update"
   # get '/sandwiches', to: 'sandwiches#index'
   # get '/sandwiches/:id', to: 'sandwiches#show', as: 'sandwich'
   resources :sandwiches
