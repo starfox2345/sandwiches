e7c82455ee3d9eb34aa2625df6abe8a35a6d70ea
yatsun <yatsunlam25@gmail.com>
2021-08-12 02:42:26 -0400

    cateogires form good to go

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -44,7 +44,7 @@ class CategoriesController < ApplicationController
    private
 
    def category_params
-        params.require(:category).permit(:name)
+        params.require(:category).permit(:name, sandwiches_attributes: [:ingredient, :price])
    end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
     has_many :sandwiches
 
-    accepts_nested_attributes_for :sandwiches
+    accepts_nested_attributes_for :sandwiches, reject_if: proc { |attributes| attributes['ingredient'].blank? || attributes['price'].blank? }
     
     validates :name, presence: true, uniqueness: {scope: [:name]}, length: {minimum: 2, message: "needs to be longer than 2 letters"}
 
