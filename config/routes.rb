506889524f90b3c394ef507a25643fe419f17263
yatsun <yatsunlam25@gmail.com>
2021-08-12 17:59:15 -0400

    nested route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # get '/sandwiches', to: 'sandwiches#index'
   # get '/sandwiches/:id', to: 'sandwiches#show', as: 'sandwich'
   resources :sandwiches
-  resources :categories
+  resources :categories do 
+    resources :sandwiches
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
