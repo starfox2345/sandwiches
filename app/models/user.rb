a878ffbd20769d25e1d024264875b3b6584e766c
yatsun <yatsunlam25@gmail.com>
2021-08-15 23:10:08 -0400

    Omniauth still having trouble

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     has_many :sandwiches, through: :orders
 
     validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+
     has_secure_password
 end
